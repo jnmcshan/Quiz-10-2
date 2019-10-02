b43eb1161d8050361d328b5040b490cb4c5a77b0
jnmcshan <54905760+jnmcshan@users.noreply.github.com>
2019-10-02 19:59:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var newString = document.body.querySelector(".newString");
+newString.innerHTML="number"
+var words = prompt("number");
+
+var number = number(prompt("number"));
+
+var str = "dog";
+
+if(isNaN(number)){
+  newString.innerHTML="Add number!");
+  
+}else{
+  for(var i = 0; i < number; i++){
+    str = str + words+ "dog"
+  }
+  newString.innerHTML=str
+}
